faa8bd9a9f1fd53854c2e4c407a8a27a8189335c
washiPolly <pollylsu@gmail.com>
2020-08-15 22:14:37 -0700

    updated server file

--- server.js
+++ server.js
@@ -3,23 +3,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://server:server123@ds237713.mlab.com:37713/heroku_zljfhgjt")
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://server:server123@ds237713.mlab.com:37713/heroku_zljfhgjt",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
-
